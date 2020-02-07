2ec1d197ecba2259e145002a55b9c56fd4af8c80
sonsation <jhson9304@gmail.com>
2020-02-07 09:47:08 -0800

    driver/base/power : introduce Kernel Wakelock Blocker Driver

---
+++ include/linux/wakelock_blocker.h
@@ -0,0 +1,37 @@
+/*
+ * Kernel Wakelock Block driver
+ *
+ * Copyright (c) 2018 Sonsation <jhson9304@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
+#ifndef __LINUX_WAKELOCK_BLOCKER_H
+#define __LINUX_WAKELOCK_BLOCKER_H
+
+#include <linux/list.h>
+
+#define MAJOR_VERSION "1.0"
+#define MAX_NAME_LEN  255
+#define DEFAULT_ENABLED true
+#define DEFAULT_LIST ""
+
+struct wake_item {
+	char wake_name[MAX_NAME_LEN];
+	size_t wake_count;
+	int len;
+	struct list_head list;
+};
+
+bool check_wakelock(struct wakeup_source *ws);
+
+#endif
